354c1fb18d2ae842435735cd9f04d13e75e0e6ee
Ziqi Liu <lzq1254307036@gmail.com>
2026-06-01 14:52:05 +0800

    debug: dev.js 加启动日志和 error 事件处理

--- scripts/dev.js
+++ scripts/dev.js
@@ -2,11 +2,18 @@ const { spawn } = require('child_process')
 const path = require('path')
 
 const electronPath = path.join(__dirname, '..', 'node_modules', 'electron', 'dist', 'electron.exe')
+console.log('[dev] Starting Electron:', electronPath)
+
 const child = spawn(electronPath, ['.'], { stdio: ['inherit', 'pipe', 'pipe'], shell: false })
 
 child.stdout.on('data', (data) => process.stdout.write(data))
 child.stderr.on('data', (data) => process.stderr.write(data))
 
+child.on('error', (err) => {
+  console.error('[dev] Failed to start Electron:', err.message)
+})
+
 child.on('close', (code) => {
+  console.log('[dev] Electron exited with code', code)
   process.exit(code ?? 0)
 })
